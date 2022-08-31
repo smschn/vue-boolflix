3268801191edb0bea512d7ec7b963282d382d3f3
Luca Somaschini <somas99@gmail.com>
2022-08-31 03:03:04 +0200

    Created components, header & main basic graphics done

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,41 @@
+<template>
+    <header>
+        <h1>BOOLFLIX</h1>
+        <div>
+            <input type="text" placeholder="Digita per cercare un film." />
+            <button>Cerca</button>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'MyHeader'
+}
+</script>
+
+<style lang='scss'>
+@import '../styles/vars.scss';
+
+header {
+    height: 80px;
+    background-color: $bg_h;
+    color: $bg_c1;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 30px;
+
+    input,
+    button {
+        padding: 10px;   
+    }
+
+    button {
+        background-color: $bg_c1;
+        border-color: $bg_c1;
+        color: #fff;
+    }
+
+}
+</style>
---
+++ src/components/MyMain.vue
@@ -0,0 +1,21 @@
+<template>
+    <main>
+        
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'MyMain'
+}
+</script>
+
+<style lang='scss'>
+@import '../styles/vars.scss';
+
+main {
+    height: calc(100vh - 80px);
+    background-color: $bg_m;
+}
+
+</style>
