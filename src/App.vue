2248e4dc67e410978f952841b649fd62b8325595
Luca Somaschini <somas99@gmail.com>
2022-09-01 04:41:48 +0200

    Milestone_2: api call for tv series in app.vue

--- src/App.vue
+++ src/App.vue
@@ -19,25 +19,37 @@ export default {
   data() {
     return {
       wordsToSearch: '',
+      apiUrl: 'https://api.themoviedb.org/3/',
       myApiKey: '3642ddadc741136c1c6eda46fa6ee412',
       language: 'it-IT',
-      moviesList: []
+      moviesList: [],
+      seriesList: []
     }
   },
   methods: {
     getWordsToSearch(words) {
       this.wordsToSearch = words;
-      this.callApi();
+      this.callApiMovies();
+      this.callApiSeries();
     },
-    callApi() {
-      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${this.myApiKey}&language=${this.language}&query=${this.wordsToSearch}`)
+    callApiMovies() {
+      axios.get(`${this.apiUrl}search/movie?api_key=${this.myApiKey}&language=${this.language}&query=${this.wordsToSearch}`)
       .then(response => {
         this.moviesList = response.data.results;
       })
       .catch(error => {
         console.log(error);
       })
-    }
+    },
+    callApiSeries() {
+      axios.get(`${this.apiUrl}search/tv?api_key=${this.myApiKey}&language=${this.language}&query=${this.wordsToSearch}`)
+      .then(response => {
+        this.seriesList = response.data.results;
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    }    
   }
 }
 </script>
